7a86fede7dd165e9c92a59b9be5be4bfa855b0ce
BBJ197 <beamlakbekele197@gmail.com>
2026-03-03 22:02:00 +0300

    Added another funstion that compairs our answers and tells us werher the user won or not

--- RPS.js
+++ RPS.js
@@ -16,6 +16,29 @@ function getHumanChoice(){
     return responce.toLowerCase()
 }
 
+function playRound(humanChoice, computerChoice) {
+    let humanScore = 0
+    let computerScore = 0
+        if (computerChoice == "rock" && humanChoice == "rock" 
+        || computerChoice == "peper" && humanChoice == "peper"
+        || computerChoice == "siasor" && humanChoice == "siasor"){
+            console.log ("Same answer retry again")
+            return "d"
+        }
+
+        else if (computerChoice == "rock" && humanChoice == "peper" 
+        || computerChoice == "peper" && humanChoice == "siasor" 
+        || computerChoice == "siasor" && humanChoice == "rock"){
+            console.log("You Won! This round " + humanChoice + " beats " + computerChoice)
+            return "w"
+        }
+
+        else {
+            console.log("You lose! This round. " + computerChoice + " beats " + humanChoice)
+            return "l"
+        }
+}
+
 
 
 console.log(getComputerChoice())
